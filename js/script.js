0364198c339b9116ec450dcae413c88a4e5ba973
annaMart <anna_tandem@mail.ru>
2021-08-14 19:34:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+"use strict"; 
+
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//   a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// var ame = 'Anna'; // ?
+
+// myName = 'Great';
+
+// let numero = 4.6;
+
+// console.log(4/0);
+// console.log('string' * 9); //NaN (not a number)
+
+// const person = `Anna`;
+
+// const object = {
+//     name: 'John',
+//     age: 50,
+//     isMarried: false
+// };
+// //console.log(object.age);
+
+// let arr = ['go', 'run', 'jump'];
+// console.log(arr[2]);
+
+// alert('Hello, Anna');
+
+
+
+// let question = confirm('Are you here?');
+// console.log(question);
+
+// let answer = prompt( 'Are you over 18?', 'yes');
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt('What`s your name?','');
+// answers[1] = prompt('What`s your last name?','');
+// answers[3] = prompt('What`s your address?','');
+
+// // document.write(answers);
+// console.log(typeof(answers));
+
+
+
+                                    //  Интерполяция 
+
+const category = 'toys';
+console.log(`http://someurl.com/${category}/5`);
+
+
+
+const user = 'Hanna';
+alert(`Hi, ${user}`);
